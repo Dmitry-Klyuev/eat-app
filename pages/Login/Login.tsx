facac2d363d063005be5a6adcf77f2b8ccfedc6f
dmitry <i6169696@gmail.com>
2024-03-10 00:18:38 +0300

    added pegister page

--- pages/Login/Login.tsx
+++ pages/Login/Login.tsx
@@ -1,17 +1,37 @@
-import {FC} from "react";
+import {FC, SyntheticEvent, useState} from "react";
 import styles from './Login.module.scss';
 import {Link} from "react-router-dom";
+import {Button} from "../../src/components/Button/Button.tsx";
+import {Input} from "../../src/components/Input/Input.tsx";
 
 export const Login: FC = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const submitForm = (e: SyntheticEvent) => {
+        e.preventDefault()
+        const formData = {email, password}
+        console.log(formData);
+    }
+
     return (
         <div className={styles['wrapper']}>
             <form action="">
                 <h2>Вход</h2>
                 <label htmlFor='email'>Ваш Email</label>
-                <input type="email" id={'email'} placeholder={'Email'}/>
+                <Input type="email"
+                       id={'email'}
+                       placeholder={'Email'}
+                       value={email}
+                       onChange={(e) => setEmail(e.target.value)}
+                />
                 <label htmlFor='password'>Ваш пароль</label>
-                <input type="password" id={'password'} placeholder={'Пароль'}/>
-                <button>Вход</button>
+                <Input type="password"
+                       id={'password'}
+                       placeholder={'Пароль'}
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button size={'large'} onClick={submitForm}>Вход</Button>
                 <span>Нет аккаунта?</span>
                 <Link to={'/auth/register'}>Зарегестрироваться</Link>
             </form>
--- pages/Register/Register.tsx
+++ pages/Register/Register.tsx
@@ -1,10 +1,28 @@
 import {FC} from "react";
 import styles from './Register.module.scss';
+import {Title} from "../../src/components/Title/Title.tsx";
+import {Input} from "../../src/components/Input/Input.tsx";
+import {Button} from "../../src/components/Button/Button.tsx";
+import {Link} from "react-router-dom";
 
 export const Register: FC = () => {
     return (
         <div className={styles['wrapper']}>
-Регистрация
+            <Title>Регистрация</Title>
+            <form className={styles['form']}>
+                <label htmlFor="email">Ваш Email</label>
+                <Input placeholder={'Email'} id={'email'}/>
+                <label htmlFor="pass">Ваш пароль</label>
+                <Input placeholder={'Пароль'} id={'pass'} type={"password"}/>
+                <label htmlFor="name">Ваше имя</label>
+                <Input placeholder={'Имя'} id={'name'}/>
+                <Button size={'large'}>Зарегестироваться</Button>
+            </form>
+            <div className={styles['footer']}>
+                <span>Есть аккаунт?</span>
+                <Link to={'/auth/login'}>Войти</Link>
+            </div>
+
         </div>
     );
 };
