0b46efb1cab3febcac21796ae58f18a0349b0456
Dmitry <i6169696@gmail.com>
2024-03-09 17:29:53 +0300

    test git

--- pages/Register/Register.tsx
+++ pages/Register/Register.tsx
@@ -2,6 +2,7 @@ import {FC} from "react";
 import styles from './Register.module.scss';
 
 export const Register: FC = () => {
+    console.log()
     return (
         <div className={styles['wrapper']}>
 Регистрация
