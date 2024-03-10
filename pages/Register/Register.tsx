2ba5775162ff960236a507a5e905bc7abb3819a8
dmitry <i6169696@gmail.com>
2024-03-10 10:41:33 +0300

    sing up and sing in

--- pages/Register/Register.tsx
+++ pages/Register/Register.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, SyntheticEvent} from "react";
 import styles from './Register.module.scss';
 import {Title} from "../../src/components/Title/Title.tsx";
 import {Input} from "../../src/components/Input/Input.tsx";
@@ -6,18 +6,32 @@ import {Button} from "../../src/components/Button/Button.tsx";
 import {Link} from "react-router-dom";
 
 export const Register: FC = () => {
+
+    const onSubmit = (e: SyntheticEvent) => {
+        e.preventDefault()
+        const target = e.target as typeof e.target & {
+            email: { value: string }
+            password: { value: string }
+            name: { value: string }
+        }
+        const name = target.name.value
+        const email = target.name.value
+        const password = target.password.value
+        console.log(name + email + password)
+    }
+
     console.log()
     return (
         <div className={styles['wrapper']}>
             <Title>Регистрация</Title>
             <form className={styles['form']}>
                 <label htmlFor="email">Ваш Email</label>
-                <Input placeholder={'Email'} id={'email'}/>
-                <label htmlFor="pass">Ваш пароль</label>
-                <Input placeholder={'Пароль'} id={'pass'} type={"password"}/>
+                <Input placeholder={'Email'} id={'email'} name={'email'}/>
+                <label htmlFor="password">Ваш пароль</label>
+                <Input placeholder={'Пароль'} id={'password'} type={'password'} name={'password'}/>
                 <label htmlFor="name">Ваше имя</label>
-                <Input placeholder={'Имя'} id={'name'}/>
-                <Button size={'large'}>Зарегестироваться</Button>
+                <Input placeholder={'Имя'} id={'name'} name={'name'}/>
+                <Button size={'large'} onClick={onSubmit}>Зарегестироваться</Button>
             </form>
             <div className={styles['footer']}>
                 <span>Есть аккаунт?</span>
